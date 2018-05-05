381b903161cbf4a896ff31b5c85fc2eb3fd95e7a
Huan LI <zixia@zixia.net>
2018-05-06 01:06:20 +0800

    lint

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 let VERSION: string
 try {
-    VERSION = require('../package.json').version
+  VERSION = require('../package.json').version
 } catch (e) {
-    VERSION = '0.0.0'
+  VERSION = '0.0.0'
 }
 
 export {
-    VERSION
+  VERSION,
 }
 
